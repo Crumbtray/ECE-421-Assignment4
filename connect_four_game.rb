1329a85dce286be69ae2b98fc72cb916d4e17bfe
Clinton Wong <clint.isaac.wong@gmail.com>
2013-11-17 12:46:01 -0700

    Hooked up the View with the Model

--- connect_four_game.rb
+++ connect_four_game.rb
@@ -1,18 +1,27 @@
 require './connect_four_game_board'
-
+require './connect_four_ui'
 
 class ConnectFourGame
-
-	def initialize(gameType)
+ 
+	attr_reader :rows, :columns, :gameBoard
+	
+	def initialize(gameType, row = 6, col = 7)
 		# Game Type is either Normal, or TOOT (OTTO)
-
-
+		@ui = ConnectFourUI.new(self)
+		@rows = row
+		@columns = col
 		@gameType = gameType
-		@gameBoard = ConnectFourGameBoard.new(6, 7)
+		@gameBoard = ConnectFourGameBoard.new(row, col)
 	end
 
+	def move(col)
+	    puts "Player tried to add a marker to column #{col}."
+	end
+	
 	def checkWinCondition
 
 	end
 
 end
+
+game = ConnectFourGame.new("Normal")
--- connect_four_ui.rb
+++ connect_four_ui.rb
@@ -1,4 +1,26 @@
 require 'gtk2'
 
-Gtk.init
-Gtk.main
+class ConnectFourUI
+  def initialize(connectFourGame)
+    @gameInstance = connectFourGame
+    Gtk.init
+    
+    button1 = Gtk::Button.new("1")
+    button1.signal_connect("clicked") {
+      connectFourGame.move(1)
+    }
+    
+    window = Gtk::Window.new
+    window.signal_connect("destroy") {
+      puts "Destroy event occurred"
+      Gtk.main_quit
+    }
+    window.border_width = 10
+    window.add(button1)
+    window.show_all
+    Gtk.main
+  end
+  
+  
+  
+end
