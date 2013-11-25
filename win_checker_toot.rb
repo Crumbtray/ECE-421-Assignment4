7007daca4ab20df8d70b5313d319c6a609c9d70e
Tyler Schneider <teschnei@gmail.com>
2013-11-24 22:43:04 -0700

    Implemented win_cheecker_toot (Supposedly.  I don't actually know how TOOT works)

--- win_checker_toot.rb
+++ win_checker_toot.rb
@@ -5,5 +5,46 @@ module WinCheckerToot
 			raise ArgumentError, "WinCheckerToot:: ArgumentError -> Game is over.  Please start a new one." unless gameBoard.endGame == false
 		end
 		# End Pre Conditions
+		
+		
+		anyOpenSpace = false
+		for x in 0..gameBoard.colSize - 1
+			for y in 0..gameBoard.rowSize - 1
+				current = gameBoard.grid[x][y]
+				#check if there are any lines
+				if(current != nil)
+					if(gameBoard.grid[x+1][y] not nil &&
+					    current != gameBoard.grid[x+1][y] &&
+						gameBoard.grid[x+2][y] not nil &&
+						current != gameBoard.grid[x+2][y] &&
+						current == gameBoard.grid[x+3][y])
+						gameBoard.endGame = true;
+						return current;
+					end
+					if(gameBoard.grid[x][y+1] not nil &&
+						current != gameBoard.grid[x][y+1] &&
+						gameBoard.grid[x][y+2] not nil &&
+						current != gameBoard.grid[x][y+2] &&
+						current == gameBoard.grid[x][y+3])
+						gameBoard.endGame = true;
+						return current;
+					end
+					if(gameBoard.grid[x+1][y+1] not nil &&
+						current != gameBoard.grid[x+1][y+1] &&
+						gameBoard.grid[x+1][y+1] not nil &&
+						current != gameBoard.grid[x+2][y+2] &&
+						current == gameBoard.grid[x+3][y+3])
+						gameBoard.endGame = true;
+						return current;
+					end
+				else
+					anyOpenSpace = true
+				end
+			end
+		end
+		if(!anyOpenSpace)
+			gameBoard.endGame = true;
+			return "draw"
+		end
 	end
 end
