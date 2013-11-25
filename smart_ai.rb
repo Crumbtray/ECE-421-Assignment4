04cb46a600a74941dc05617acbb1a7beff6911ff
cbeckett <cbeckett@ualberta.ca>
2013-11-25 08:49:35 -0700

    Mostly finished.  Has pending accessor bugs on the game state as well as a the deep copy of the game state not working correctly

--- smart_ai.rb
+++ smart_ai.rb
@@ -1,24 +1,81 @@
 class SmartAI
 	def initialize
-		#Initialize heuristics values from point of view of player and opponent
 	end
 
-	#SmartAI uses heuristics to choose its next move
-	def makeMove(gameBoard)
+	#SmartAI chooses either a winning move, blocking move, or a move that is the minimal steps away from winning
+	def makeMove(gameInstance)
+	possibleMoves = Array.new
+		gameBoard = gameInstance.gameBoard.clone
+		winChecker = gameInstance.winChecker
+		
 		# Pre Conditions
 		begin
-			raise ArgumentError, "DumbAI:: ArgumentError -> Game is over.  Please start a new one." unless gameBoard.endGame == false
+			raise ArgumentError, "SmartAI:: ArgumentError -> Game is over.  Please start a new one." unless gameBoard.endGame == false
 		end
 		# End Pre Conditions
 		
-		#For each column (possible move)
-			#Get row heuristics
-			#Get column heuristics
-			#Get down diagonal heuristics
-			#Get up diagonal heurisitics
-				
-		#Repeat, get heuristics from point of view of opponent
+		#Determine who our opponent is
+		if(gameBoard.player1 == self)
+			opponent = gameBoard.player2
+		else
+			opponent = gameBoard.player1
+		end
+			
+		#Analyse each column (move)
+		for move in 0..gameBoard.colSize - 1
+			#Check if a move here will cause player to win
+			boardCopy = gameBoard.clone
+			boardCopy.currentPlayer = self
+			boardCopy.add(self, move + 1)
+			if(winChecker.checkWinCondition(boardCopy))
+				possibleMoves[move] = -1000
+				break
+			end
+			
+			#Check if move here will block opponent win
+			boardCopy = gameBoard.clone
+			boardCopy.currentPlayer = opponent
+			boardCopy.add(opponent, move + 1)
+			if(winChecker.checkWinCondition(boardCopy))
+				possibleMoves[move] = -1000
+				break
+			end
+			
+			#Check how many moves away we are from a win here
+			possibleMoves[move] = getMoveDepth(gameBoard, winChecker)
+		end
 		
-		#Choose max valued move compared between the max value of our heuristics and the opponents heuristics
+		#Choose the "best" move (ties broken randomly)
+		bestMove = (possibleMoves.each.with_index.find_all{ |a, i| a == possibleMoves.min}.map{|a,b| b}).sample
+		return gameInstance.move(self, bestMove)
+	end
+	
+	def getMoveDepth(gameBoard, winChecker)
+		#Check if we have made a winning move
+		if(winChecker.checkWinCondition(gameBoard))
+			return 0
+		end
+		
+		#Check if there are anymore moves to make
+		if(gameBoard.endGame == true)
+			return 0
+		end
+		
+		possibleMoves = Array.new
+		for move in 0..gameBoard.colSize - 1
+			if(gameBoard.grid[move].size < gameBoard.colSize)
+				boardCopy = gameBoard.clone
+				boardCopy.currentPlayer = self
+				boardCopy.add(self, move + 1)
+				possibleMoves[move] = 1 + getMoveDepth(boardCopy, winChecker)
+			end
+		end
+		
+		#Choose the minimum depth (ties broken randomly)
+		return (possibleMoves.each.with_index.find_all{ |a, i| a == possibleMoves.min}.map{|a,b| b}).sample
+	end
+	
+	def updateGrid(location, player)
+
 	end
 end
